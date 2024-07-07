cd11b8e5b3b5637b3f4bfc575a7095dc1251805f
m-shazaib <shahzaib3742@gmail.com>
2024-07-07 16:28:15 +0500

    new poll creation

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,11 +1,15 @@
 import { Stack } from "expo-router";
 import HomeScreen from ".";
 import AuthProvider from "../provider/AuthProvider";
+import Auth from "./(auth)/login";
 
 export default function RootLayout() {
     return (
         <AuthProvider>
-            <Stack />
+            <Stack >
+                <Stack.Screen name="(auth)" options={{title: "Login"}}/>
+                <Stack.Screen name="(protected)" options={{title: "Profile"}}/>
+            </Stack>
         </AuthProvider>
     )
 }
--- src/app/polls/newPoll.tsx
+++ src/app/polls/newPoll.tsx
@@ -1,15 +1,54 @@
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native'
-import React, { useState } from 'react'
+import { Alert, Button, StyleSheet, Text, TextInput, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import { Entypo } from '@expo/vector-icons';
-import { Stack } from 'expo-router'
+import { router, Stack } from 'expo-router'
+import { useAuth } from '../../provider/AuthProvider';
+import { supabase } from '../../lib/supabase';
 
 type Props = {}
 
 const CreatePoll = (props: Props) => {
     const [question, setQuestion] = useState('');
     const [options, setOptions] = useState(['']);
+    const [error, setError] = useState('');
+    const { user } = useAuth();
 
-    const createPoll = () => {
+
+    // useEffect(() => {
+    //     if (!user) {
+    //         Alert.alert('Error', 'You must be logged in to create a poll');
+    //         router.push('/login');
+    //         return;
+    //     }
+    // }, [user]);
+
+    const createPoll = async () => {
+        if(!user) {
+            Alert.alert('Error', 'You must be logged in to create a poll');
+            return;
+        }
+        if (!question) {
+            setError('Question is required');
+            return;
+        }
+
+        const validOptions = options.filter((o) => !!o);
+
+        if (validOptions.length < 2) {
+            setError('At least two options are required');
+            return;
+        }
+
+        const { data, error } = await supabase
+            .from('Poll')
+            .insert([{ question, option: validOptions }]) 
+            .select();
+
+        if (error) {
+            console.log(error);
+            return;
+        }
+        router.back();
         console.warn('Create Poll');
     };
 
@@ -24,32 +63,46 @@ const CreatePoll = (props: Props) => {
                 headerTintColor: 'white',
             }} />
             <Text style={styles.label}>Title</Text>
-            <TextInput style={styles.input} value={question} onChangeText={setQuestion} placeholder='Type your question here' />
+            <TextInput
+                style={styles.input}
+                value={question}
+                onChangeText={setQuestion}
+                placeholder='Type your question here'
+            />
 
-
-            <Text style={styles.label}>Option</Text>
+            <Text style={styles.label}>Options</Text>
             {options.map((option, index) => (
                 <View key={index} style={{ justifyContent: "center" }}>
-                    <TextInput style={styles.input} value={option} onChangeText={(text) => {
-                        const newOptions = [...options];
-                        newOptions[index] = text;
-                        setOptions(newOptions);
-                    }} placeholder={`Option ${index + 1}`} />
-                    <Entypo style={{position: "absolute",
-                     right: 10 }}  name="cross" size={24} color="black" 
+                    <TextInput
+                        style={styles.input}
+                        value={option}
+                        onChangeText={(text) => {
+                            const updated = [...options];
+                            updated[index] = text;
+                            setOptions(updated);
+                        }}
+                        placeholder={`Option ${index + 1}`}
+                    />
+                    <Entypo
+                        style={{
+                            position: "absolute",
+                            right: 10
+                        }}
+                        name="cross"
+                        size={24}
+                        color="black"
                         onPress={() => {
-                            const newOptions = [...options];
-                            newOptions.splice(index, 1);
-                            setOptions(newOptions);
-                        }
-                        }
+                            const updated = [...options];
+                            updated.splice(index, 1);
+                            setOptions(updated);
+                        }}
                     />
                 </View>
             ))}
+
             <Button title='Add Option' onPress={() => setOptions([...options, ''])} />
-
             <Button title='Create Poll' onPress={createPoll} />
-
+            <Text style={{ color: 'red' }}>{error}</Text>
         </View>
     )
 }
@@ -71,5 +124,4 @@ const styles = StyleSheet.create({
         padding: 10,
         borderRadius: 5,
     },
-
 })
